5e6a7df0a3fddce236ac34293fd1df3554b77534
prashu3324 <prashanth9948@gmail.com>
2022-08-20 19:16:07 +0000

    diff added

--- app.py
+++ app.py
@@ -15,3 +15,10 @@ prod = num1*num2
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, prod))
 
+# sub two numbers
+sub  = num2 - num1
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, diff))
+
+
